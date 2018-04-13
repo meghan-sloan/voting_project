72505ab9de28fee2eeda74149131a4107f11e769
Meghan Sloan <meghan.m.sloan@gmail.com>
2018-04-13 09:54:43 -0600

    made mk_dict function to clean up if main block

--- merging_data.py
+++ merging_data.py
@@ -25,27 +25,41 @@ def get_precinct_address(x, precinct_dict, column):
     except KeyError:
         return 'NOT AVAILABLE'
 
+def mk_polling_dict(df):
+    '''
+    Formats the polling data frames so the precinct ID is the same format
+    as the addresses.csv precinct ID.
+    Creates a dictionary of the polling location data using the precinct ID as the key
+    '''
+    # Create a new column with just the state
+    df['state'] = df['State/ZIP'].apply(lambda x: x.split()[0])
+    # Create a new column with just the zip
+    df['zip'] = df['State/ZIP'].apply(lambda x: x.split()[-1])
+    # Turn the state abbreviation into a 3 digit code
+    df['state_code'] = df['state'].apply(lambda x: new_code(x))
+    # Get the second half of the precinct code and ensure it is 3 digits
+    df['precinct_code'] = df['Precinct'].apply(lambda x: precinct_code(x))
+    # Combine the state and precinct codes
+    df['new_code'] = df['state_code']+'-'+df['precinct_code']
+    # Turn the polling dataframe into a dictionary
+    new_polling = df.set_index('new_code')
+    precinct_dict = new_polling.to_dict('index')
+    return precinct_dict
+
+
 if __name__ == '__main__':
     #Create dataframes for the csvs
     df_address = pd.read_csv('/Users/meghan/DemocracyWorks/data/addresses_fixed.csv', delimiter=';')
     df_polling = pd.read_csv('/Users/meghan/DemocracyWorks/data/precinct_polling_fixed.csv', delimiter=';')
-    # Create a new column with just the state
-    df_polling['state'] = df_polling['State/ZIP'].apply(lambda x: x.split()[0])
-    # Create a new column with just the zip
-    df_polling['zip'] = df_polling['State/ZIP'].apply(lambda x: x.split()[-1])
-    # Turn the state abbreviation into a 3 digit code
-    df_polling['state_code'] = df_polling['state'].apply(lambda x: new_code(x))
-    # Get the second half of the precinct code and ensure it is 3 digits
-    df_polling['precinct_code'] = df_polling['Precinct'].apply(lambda x: precinct_code(x))
-    # Combine the state and precinct codes
-    df_polling['new_code'] = df_polling['state_code']+'-'+df_polling['precinct_code']
-    # Turn the polling dataframe into a dictionary
-    new_polling = df_polling.set_index('new_code')
-    precinct_dict = new_polling.to_dict('index')
+
+    # Create a dictionary of precint polling data
+    precinct_dict = mk_polling_dict(df_polling)
+
     # Add the polling address to the address dataframe
     df_address['polling_street'] = df_address['Precinct ID'].apply(lambda x: get_precinct_address(x, precinct_dict, 'Street'))
     df_address['polling_city'] = df_address['Precinct ID'].apply(lambda x: get_precinct_address(x, precinct_dict, 'City'))
     df_address['polling_zip'] = df_address['Precinct ID'].apply(lambda x: get_precinct_address(x, precinct_dict, 'zip'))
     df_address['precinct_code'] = df_address['Precinct ID'].apply(lambda x: get_precinct_address(x, precinct_dict, 'precinct_code'))
 
+    # Save to a csv
     df_address.to_csv('/Users/meghan/DemocracyWorks/combined.csv')
